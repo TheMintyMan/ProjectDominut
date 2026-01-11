8a3a8ab51f2395b0320f6763db6963d54c2ab2d9
TheMintyMan <william.mintie@gmail.com>
2026-01-11 16:31:37 +1100

    updated global.gd. switch levels. UI

--- scripts/global.gd
+++ scripts/global.gd
@@ -5,12 +5,19 @@ extends Node
 const inital_currency: int = 10
 var current_currency: int = inital_currency
 
-# DONUT VALUES
+var levels = [
+	"res://art/ui/main_menu.tscn",
+	"res://scenes/Map_03.tscn",
+]
+var level_index: int = 0
 
-enum ResistanceType{
-	NONE,
-	SHOOTER,
-	AREA,
-	PLACING,
-	WALL,
-}
+func next_level():
+	level_index = (level_index + levels.size() + 1) % levels.size()
+	get_tree().change_scene_to_file(levels[level_index])
+	
+func previous_level():
+	level_index = (level_index + levels.size() - 2) % levels.size()
+	get_tree().change_scene_to_file(levels[level_index])
+
+func restart_level():
+	get_tree().reload_current_scene()
